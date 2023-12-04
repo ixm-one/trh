38f01da9abb656954a553ced5344762b0a9c4753
Izzy Muerte <63051+bruxisma@users.noreply.github.com>
2023-12-03 20:16:25 -0800

    🚧 Continuing to flesh out some of the SNES related tooling.
    Trying to stay as generic as possible so that if someone wants to write
    better tools it'll be easier for them to be dropped in.
    
    Signed-off-by: Izzy Muerte <63051+bruxisma@users.noreply.github.com>

--- Modules/Platform/SNES.cmake
+++ Modules/Platform/SNES.cmake
@@ -1,3 +1,10 @@
-set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
-
 # determine define with __65816__ and __TINYC__
+
+set(CMAKE_SYSTEM_INCLUDE_PATH
+  "${CMAKE_SNES_ROOT}/devkitsnes/include"
+  "${CMAKE_SNES_ROOT}/pvsneslib/include")
+set(CMAKE_SYSTEM_PROGRAM_PATH
+  "${CMAKE_SNES_ROOT}/devkitsnes/bin"
+  "${CMAKE_SNES_ROOT}/devkitsnes/tools")
+set(CMAKE_SYSTEM_LIBRARY_PATH
+  "${CMAKE_SNES_ROOT}/pvsneslib/lib")
